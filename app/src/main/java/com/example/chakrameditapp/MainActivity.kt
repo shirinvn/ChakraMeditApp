70a50d33e21bcc73826db2d277fd522a68bc653e
Vatani <shirinvatani97@gmail.com>
2023-07-15 14:32:05 +0330

    Fragments

---
+++ app/src/main/java/com/example/chakrameditapp/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.chakrameditapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.replace
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.chakrameditapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+   private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNav.selectedItemId =R.id.home
+        binding.bottomNav.setOnItemSelectedListener {
+            when (it.itemId){
+
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.setting -> replaceFragment(SettingFragment())
+                R.id.book -> replaceFragment(BookFragment())
+
+                else -> {
+
+                }
+
+            }
+            true
+        }
+
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager= supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+    }
+}
